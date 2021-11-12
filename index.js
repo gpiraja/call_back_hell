1a9d2c27af756df9b591db858c335b5f04bf82a9
gpiraja <germanopiraja@gmail.com>
2021-11-12 11:15:50 -0300

    Conexao entre node js e mongo bd

---
+++ index.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017/';
+const dbname = 'conFusion';
+
+MongoClient.connect(url, (err, client) => {
+    assert.equal(err, null);
+    console.log('Conectado corretamente no servidor');
+
+    const db = client.db(dbname);
+    const collection = db.collection("receitas");
+
+    collection.insertOne ({"nome" : "Joao", "desc": "Cara de mamão"},
+    (err, result) => {
+        assert.equal(err, null);
+        console.log('Registro adicionado ao db: \n');
+        console.log(result.insertedId);
+
+        collection.find({}).toArray((err, docs) =>{
+            assert.equal(err, null);
+
+            console.log("Registros db:\n");
+            console.log(docs);
+
+            //db.dropCollection("receitas", (err, result) =>{
+              //  assert.equal(err, null);
+               // client.close();
+            //})
+        });
+    });
+});
+
+
