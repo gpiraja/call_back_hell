bc332b3a7066f97d8ec2f4f1305d408af00998be
gpiraja <germanopiraja@gmail.com>
2021-11-14 16:16:11 -0300

    Operaçoes no Mongo separadas em arquivos proprio

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations')
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -11,25 +12,26 @@ MongoClient.connect(url, (err, client) => {
     const db = client.db(dbname);
     const collection = db.collection("receitas");
 
-    collection.insertOne({
-            "nome": "Joao",
-            "desc": "Cara de mamão"
-        },
-        (err, result) => {
-            assert.equal(err, null);
-            console.log('Registro adicionado ao db: \n');
-            console.log(result.insertedId);
+    dboper.insertDocument(db, {nome : "Vadio", desc: "teste"},
+        "receitas", (result) => {
+            console.log("Inserindo receita ", result.ops);
+            
+            dboper.findDocuments(db, "receitas", (docs) => {
+                console.log("Achados: ", docs);
 
-            collection.find({}).toArray((err, docs) => {
-                assert.equal(err, null);
+                dboper.removeDocument (db, {nome : "Vadio", desc: "teste"}, 
+                "receitas",(result) =>{
+                    console.log("apagado ", result);
+                    db.dropCollection("receitas", (result) =>{
+                        console.log("Coleção apagada", result);
 
-                console.log("Registros db:\n");
-                console.log(docs);
-
-                db.dropCollection("receitas", (err, result) => {
-                    assert.equal(err, null);
-                    client.close();
-                })
+                        client.close()
+                    })
+                } )
             });
-     });
+
+
+        })
+
+    
 });
